ea5d11a22068e239666ecd77ca6ec72e5da49cb4
TAKAI Yosiyuki <ypsilon.takai@gmail.com>
2018-04-10 02:06:50 +0900

    Raise と Lowerを親指の内側にも配置 と GUIキー対応

--- keyboards/planck/keymaps/yosi/keymap.c
+++ keyboards/planck/keymaps/yosi/keymap.c
@@ -67,7 +67,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,    KC_DEL,  KC_BSPC, KC_F,    KC_G,    KC_C,    KC_R,    KC_L},
     {KC_A,    KC_O,    KC_E,    KC_U,    KC_I,    KC_TAB,  KC_ENT,  KC_D,    KC_H,    KC_T,    KC_N,    KC_S},
     {KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,    SFT_T(KC_EQL),  SFT_T(KC_MINS), KC_B,    KC_M,    KC_W,    KC_V,    KC_Z},
-    {LT(_LOWER,KC_GRV), KC_LGUI, KC_LALT, LSFT_T(KC_SPC), KC_LCTL, RAISE, LOWER, KC_RALT, RSFT_T(KC_SPC), KC_RCTL, KC_BSLS, KC_SLSH}
+    {LT(_LOWER,KC_GRV), KC_LGUI, RAISE, LSFT_T(KC_SPC), KC_LCTL, RAISE, LOWER, KC_RALT, RSFT_T(KC_SPC), LOWER, KC_BSLS, KC_SLSH}
 },
 
 /* Lower
@@ -85,7 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     {KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_INS,  KC_BSPC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN,  KC_RPRN},
     {KC_ESC,  KC_LBRC, KC_RBRC, KC_LPRN,  KC_RPRN, __XXX__, KC_ENT,  __XXX__, KC_LEFT, KC_UP,   KC_RGHT, KC_UNDS},
     {__XXX__, __XXX__, __XXX__, KC_LCBR, KC_RCBR, KC_EQL,  KC_MINS, __XXX__, KC_LEFT, KC_DOWN, KC_RGHT,  KC_QUES},
-    {_______,  KC_LSFT, KC_LALT, LSFT_T(KC_SPC), KC_LCTL, RAISE, LOWER, KC_RALT, RSFT_T(KC_SPC), KC_RCTL, KC_RSFT, _______}    
+    {_______,  KC_LGUI, _______, LSFT_T(KC_SPC), KC_LCTL, RAISE, LOWER, KC_RALT, RSFT_T(KC_SPC), _______, KC_RSFT, _______}    
 },
 
 /* Raise
@@ -103,7 +103,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   {KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    __XXX__,  __XXX__,     KC_6,    KC_7,    KC_8,    KC_9,     KC_0},
   {KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   __XXX__,  __XXX__,  __XXX__,    KC_4,    KC_5,    KC_6,    __XXX__},
   {KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,   KC_F12,  __XXX__,    KC_1,    KC_2,    KC_3,    __XXX__},
-  {_______, KC_LSFT, KC_LALT, KC_LCTL, SFT_T(KC_SPC), RAISE, LOWER, __XXX__,    KC_0, KC_COMM,  KC_DOT,    _______}
+  {_______, KC_LGUI, _______, KC_LCTL, SFT_T(KC_SPC), RAISE, LOWER, __XXX__,    KC_0, KC_COMM,  _______, _______}
 },
 
 /* Adjust (Lower + Raise)
