abe805576859c15753fa5343fd31ea4a8caec130
yosi <ypsilon.takai@gmail.com>
2020-09-14 18:29:14 +0900

    follow original changes

--- keyboards/ergodox_ez/keymaps/default/keymap.c
+++ keyboards/ergodox_ez/keymaps/default/keymap.c
@@ -206,3 +206,35 @@ layer_state_t layer_state_set_user(layer_state_t state) {
 
   return state;
 };
+
+
+#ifdef OLED_DRIVER_ENABLE
+void oled_task_user(void) {
+    // Host Keyboard Layer Status
+
+    oled_clear()
+    
+    oled_write_P(PSTR("Layer: "), false);
+
+    switch (get_highest_layer(layer_state)) {
+    case BASE:
+        oled_write_P(PSTR("Default\n"), false);
+        break;
+    case SYMB:
+        oled_write_P(PSTR("Symbol\n"), false);
+        break;
+    case MDIA:
+        oled_write_P(PSTR("Media\n"), false);
+        break;
+    default:
+        // Or use the write_ln shortcut over adding '\n' to the end of your string
+        oled_write_ln_P(PSTR("Undefined"), false);
+    }
+
+    // Host Keyboard LED Status
+    led_t led_state = host_keyboard_led_state();
+    oled_write_P(led_state.num_lock ? PSTR("NUM ") : PSTR("    "), false);
+    oled_write_P(led_state.caps_lock ? PSTR("CAP ") : PSTR("    "), false);
+    oled_write_P(led_state.scroll_lock ? PSTR("SCR ") : PSTR("    "), false);
+}
+#endif
